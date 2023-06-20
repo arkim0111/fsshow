2f261d5b53d3c57ce6ee68f85ec078f2b1fe53f6
arkim0111 <aramingk95@gmail.com>
2023-06-20 21:29:53 +0900

    spring 파일 수정

--- src/main/java/com/myproject/fsshow/show/ShowDAO.java
+++ src/main/java/com/myproject/fsshow/show/ShowDAO.java
@@ -1,4 +1,15 @@
 package com.myproject.fsshow.show;
 
-public class ShowDAO {
+import com.myproject.fsshow.dao.AbstractDAO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("showDAO")
+public class ShowDAO extends AbstractDAO {
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+
 }
--- src/main/java/com/myproject/fsshow/show/ShowDTO.java
+++ src/main/java/com/myproject/fsshow/show/ShowDTO.java
@@ -1,4 +1,13 @@
 package com.myproject.fsshow.show;
 
 public class ShowDTO {
+
+    private String SHOW_ID; // 쇼 아이디
+    private int SHOW_YEAR; // 개최 연도
+    private String SHOW_NAME; // 쇼 이름
+    private String SHOW_ADDR; // 개최 장소
+    private String SHOW_DATE; // 개최 날짜
+    private String SHOW_POST_IMG; // 포스터 이미지
+    private String SHOW_SEAT_IMG; // 좌석배치도 이미지
+
 }
